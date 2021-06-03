0480187aef83db73f4945b098f097a450f45905d
Jan-Erik Rediger <janerik@fnordig.de>
2021-06-03 14:15:57 +0200

    Bumped version to 39.0.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.4
 import PackageDescription
 
-let checksum = "50de2ff5285f40048b108caf9199410fc25304137e4664b60cec8fd01e3dd15c"
-let version = "38.0.2"
+let checksum = "379c84df0a117bc74d5dc5860eeb4172ea7cacf99457c0e8cfe4a787748dac39"
+let version = "39.0.0"
 let url = "https://github.com/badboy/glean-swift/releases/download/\(version)/Glean.xcframework.zip"
 
 let package = Package(
